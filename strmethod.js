1ada02578d5bc986cb553b344742fb93d4cf79d1
devansh <dsppatel.149@gmail.com>
2024-06-10 23:54:32 +0530

    string method

---
+++ strmethod.js
@@ -0,0 +1,16 @@
+//  string interpolation
+let name ="dev"
+let platform ="gITHUb"
+console.log(`hello my name is ${name} here i will starting coding on ${platform.toLowerCase(platform)}`);
+console.log("---------------------------------------------------------------")
+console.log(`My name is ${name}.I am pushing the code on a ${platform.toLowerCase(platform)}.`);
+console.log("---------------------------------------------------------------")
+console.log(`${name} patel from ahmedabad to ${platform.toLowerCase(platform)}`);
+console.log("---------------------------------------------------------------")
+//  string methods
+const gameName = new String ("dvs");
+console.log(gameName.length)
+console.log(gameName.__proto__)
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf("s"));
